0dfd91b38ce55f0b8db8b575550c01cb7398a60f
Tom Callahan <tom.callahan@elastic.co>
2018-05-01 10:42:15 -0400

    blah

--- server/src/main/java/org/elasticsearch/bootstrap/ShortcuttingPermissionsCollections.java
+++ server/src/main/java/org/elasticsearch/bootstrap/ShortcuttingPermissionsCollections.java
@@ -6,12 +6,17 @@ import java.io.FilePermission;
 import java.security.Permission;
 import java.security.PermissionCollection;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @SuppressForbidden(reason = "whatever")
 public class ShortcuttingPermissionsCollections extends PermissionCollection {
-    private final CopyOnWriteArrayList<PermissionCollection> perms = new CopyOnWriteArrayList<>();
+    private static final int THRESHOLD = 10_000;
+    private volatile int accesses = 0;
+    private volatile CopyOnWriteArrayList<PermissionCollectionContainer> perms = new CopyOnWriteArrayList<>();
     private final PermissionCollection delegate;
     int permNo = 0;
     int delegateNo = 0;
@@ -25,7 +30,8 @@ public class ShortcuttingPermissionsCollections extends PermissionCollection {
             FilePermission fp = (FilePermission) permission;
             PermissionCollection pc = fp.newPermissionCollection();
             pc.add(fp);
-            perms.add(pc);
+            PermissionCollectionContainer pcc = new PermissionCollectionContainer(pc);
+            perms.add(pcc);
             permNo++;
         }
         delegate.add(permission);
@@ -38,10 +44,14 @@ public class ShortcuttingPermissionsCollections extends PermissionCollection {
     @Override
     public boolean implies(Permission permission) {
         int x=0;
+        accesses++;
+        if (accesses == THRESHOLD) {
+            reorder();
+        }
         if (permission instanceof FilePermission) {
-            for (PermissionCollection pc : perms) {
+            for (PermissionCollectionContainer pcc : perms) {
                 x++;
-                if (pc.implies(permission)) {
+                if (pcc.perm.implies(permission)) {
                     if (r.nextInt(50) == 0) {
                         System.out.println("checks: "+x+ " " + permission.getName());
                     }
@@ -51,9 +61,23 @@ public class ShortcuttingPermissionsCollections extends PermissionCollection {
         }
         return delegate.implies(permission);
     }
+    private void reorder() {
+        List<PermissionCollectionContainer> list = perms.stream()
+            .sorted((a, b) -> b.accesses.get() - a.accesses.get()).collect(Collectors.toList());
+        perms = new CopyOnWriteArrayList<>(list);
+        System.out.println("re-ordered");
+    }
 
     @Override
     public Enumeration<Permission> elements() {
         return delegate.elements();
+    }
+
+    static class PermissionCollectionContainer {
+        public PermissionCollectionContainer(PermissionCollection perm) {
+            this.perm = perm;
+        }
+        private final PermissionCollection perm;
+        private final AtomicInteger accesses = new AtomicInteger(0);
     }
 }
