902117e1074390304a3b58508257ae82f54db095
Tom Callahan <tom.callahan@elastic.co>
2018-05-01 10:08:55 -0400

    blah

--- server/src/main/java/org/elasticsearch/bootstrap/ShortcuttingPermissionsCollections.java
+++ server/src/main/java/org/elasticsearch/bootstrap/ShortcuttingPermissionsCollections.java
@@ -1,14 +1,21 @@
 package org.elasticsearch.bootstrap;
 
+import org.elasticsearch.common.SuppressForbidden;
+
 import java.io.FilePermission;
 import java.security.Permission;
 import java.security.PermissionCollection;
 import java.util.Enumeration;
+import java.util.Random;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+@SuppressForbidden(reason = "whatever")
 public class ShortcuttingPermissionsCollections extends PermissionCollection {
     private final CopyOnWriteArrayList<PermissionCollection> perms = new CopyOnWriteArrayList<>();
     private final PermissionCollection delegate;
+    int permNo = 0;
+    int delegateNo = 0;
+    Random r = new Random();
     public ShortcuttingPermissionsCollections(PermissionCollection delegate) {
         this.delegate = delegate;
     }
@@ -19,15 +26,25 @@ public class ShortcuttingPermissionsCollections extends PermissionCollection {
             PermissionCollection pc = fp.newPermissionCollection();
             pc.add(fp);
             perms.add(pc);
+            permNo++;
         }
         delegate.add(permission);
+        delegateNo++;
+
+        System.out.println("shortcut perms: "+permNo);
+        System.out.println("delegate num: "+delegateNo);
     }
 
     @Override
     public boolean implies(Permission permission) {
+        int x=0;
         if (permission instanceof FilePermission) {
             for (PermissionCollection pc : perms) {
+                x++;
                 if (pc.implies(permission)) {
+                    if (r.nextInt(50) == 0) {
+                        System.out.println("checks: "+x);
+                    }
                     return true;
                 }
             }
